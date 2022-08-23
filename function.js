fd582d811a527dc7fa095ff2e480bff5a95b6c2d
sms-Sifat-115 <smssifat115@gmail.com>
2022-08-23 15:32:43 +0600

    Fourth Draft: Cal-btn Function added

---
+++ function.js
@@ -0,0 +1,13 @@
+
+
+
+
+// Budget Calculation
+document.getElementById('btn-budget').addEventListener('click', function(){
+    const inputField = document.getElementById('input-field');
+    const newInputFieldAmountString = inputField.value;
+    const newInputFieldAmount = parseFloat(newInputFieldAmountString);
+
+    inputField.value = '';
+    
+})
